f2fd3bef97ab63ed2dfb3ec1dccebb7ed03832c6
NickHe <zcabebx@ucl.ac.uk>
2023-02-16 23:22:31 +0000

    do not update bug

--- src/components/ExportButton/index.js
+++ src/components/ExportButton/index.js
@@ -1,14 +1,47 @@
-import { usePDF, Document, Page,Text } from '@react-pdf/renderer';
+import { usePDF, Document, Page,Text, PDFDownloadLink } from '@react-pdf/renderer';
 import styles from "./styles.module.scss";
 import React,{useEffect} from "react";
 
-const MyDoc = ({questionCards}) =>{
-  console.log("export questions:"+questionCards)
-  // const [names, setNames] = React.useState([]);
-  const names = React.useRef([]);
-  console.log("export question names:"+names.current)
+const MyDoc = ({qnames}) =>{
+  console.log("MyDoc: "+qnames)
+ return (
+ <Document>
+  <Page>
+      <Text>Survey tool export</Text>
+      <Text>------------------</Text>
+      {qnames.map((name,i) => (
+        <Text key={i}>{name}</Text>
+      ))}
+            
+  </Page>
+ </Document>
+ );
+}
 
-  const [qnames, setQnames] = React.useState([]);
+////////////////////////////////////////  APP  ////////////////////////////////////////
+
+const App = ({questionCards}) => {
+
+  const names = React.useRef([]);
+  // const [qnames, setQnames] = React.useState([]);
+  
+  const [instance, updateInstance] = usePDF({ document: <MyDoc qnames={names.current} /> });
+
+
+  if (instance.loading) return <div>Loading ...</div>;
+
+  if (instance.error) return <div>Something went wrong: {instance.error}</div>;
+
+
+  const update = async() => {
+    updateInstance(); 
+  }
+
+  const open = async() => {
+    window.open(instance.url);
+  }
+
+
 
   const fetchState = async(id) => {
     await fetch('https://ifrc-sampling.azurewebsites.net/api/decision-tree/'+id+'/')
@@ -24,70 +57,23 @@ const MyDoc = ({questionCards}) =>{
 
   async function resetNames() {
     names.current = [];
+    return names.current;
   }
 
-  useEffect( () =>{
-     const fetchAll = async() => {
-      await resetNames();
-      for (const id of questionCards) {
-        await fetchState(id);
-      }
+
+  const fetchAll = async() => {
+    await resetNames();
+    for (const id of questionCards) {
+      await fetchState(id);
     }
 
-    fetchAll()
-    .then(()=>{setQnames(names.current)})
-    .then(()=>{console.log("finish fetching")})
-
-  },[questionCards])
-
-  
-
- return (
- <Document>
-  <Page>
-      <Text>Survey tool export</Text>
-      <Text>------------------</Text>
-      {qnames.map((name,i) => (
-        <Text key={i}>{name}</Text>
-      ))}
-            
-  </Page>
- </Document>
- );
-}
-
-
-
-const App = ({questionCards}) => {
-  
-  const [instance, updateInstance] = usePDF({ document: <MyDoc questionCards={questionCards} /> });
-
-  
-
-  if (instance.loading) return <div>Loading ...</div>;
-
-  if (instance.error) return <div>Something went wrong: {instance.error}</div>;
-
-
-  async function handleClick() {
-    console.log("start update")
-    await update().then(()=>{console.log("finish update")}).then(()=>{open()})
-    
-  }
-
-  async function handleAwaitClick() {
     await update();
     await open();
   }
+  
 
 
-  const update = async() => {
-    updateInstance(); 
-    return("finish update")
-  }
-  const open = async() => {
-    window.open(instance.url);
-  }
+
 
 
   return (
@@ -100,12 +86,15 @@ const App = ({questionCards}) => {
       </button>
     {/* <a href={instance.url} download="test.pdf" > */}
     <a>
-      <button onClick={handleAwaitClick}>
+      <button onClick={()=>{fetchAll()}}>
         export report
       </button>
     </a>
     </div>
   );
+
+
 }
+
 
 export default App;
