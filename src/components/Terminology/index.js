22d0bdd55e6a3bf222f64a567b76a95be0eb32cf
Matyas Huba <hubamatyas@gmail.com>
2023-02-23 19:03:21 +0000

    Refactored Terminology to use functional components and ES6 syntax

--- src/components/Terminology/index.js
+++ src/components/Terminology/index.js
@@ -1,43 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { definitions } from "../Definitions";
 
-class Terminology extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            term: props.term,
-            text: props.text,
-            defintion: null,
-        };
-        this.componentDidMount = this.componentDidMount.bind(this);
-    }
+const Terminology = ({ term, text }) => {
+    const [definition, setDefinition] = useState(null);
 
-    componentDidMount() {
-        if (this.state.term in definitions) {
-            this.setState({ defintion: definitions[this.state.term] })
+    useEffect(() => {
+        if (term in definitions) {
+            setDefinition(definitions[term]);
         }
-    }
+    }, [term]);
 
-    render() {
-        const term = this.state.term;
-        const text = this.state.text;
-        const regex = new RegExp(`(${term})`, "gi");
-        const parts = this.state.text.split(regex);
-        return (
-            <>
-                {this.state.defintion ?
-                    // add Offcanvas/Tooltip component when ready
-                    <>
-                        {parts.length > 1 ?
-                        <span>{parts.map(part => part.toLowerCase() === term.toLowerCase() ? <u>{part}</u> : part)}</span>
-                        : <u>{text}</u>
-                        }
-                    </>
-                    : <> {text} </>
-                }
-            </>
-        )
-    }
-}
+    const regex = new RegExp(`(${term})`, "gi");
+    const parts = text.split(regex);
+
+    return (
+        <>
+            {definition ? (
+                // add Offcanvas/Tooltip component when ready
+                <>
+                    {parts.length > 1 ? (
+                        <span>
+                            {parts.map((part) =>
+                                part.toLowerCase() === term.toLowerCase() ?
+                                ( <u>{part}</u> ) : (part)
+                            )}
+                        </span>
+                    ) : (
+                        <u>{text}</u>
+                    )}
+                </>
+            ) : (
+                <> {text} </>
+            )}
+        </>
+    );
+};
 
 export default Terminology;
