79bd0527ee1a8c9486e4d5ed305892331d627463
Matyas Huba <hubamatyas@gmail.com>
2023-02-23 19:24:56 +0000

    Refactored QuestionCard to use functional components and ES6 syntax

--- src/components/QuestionCard/index.js
+++ src/components/QuestionCard/index.js
@@ -1,88 +1,76 @@
-import React,{useEffect} from "react";
-import styles from "./styles.module.scss";
-import Card from "../Card";
+import React, { useState, useEffect } from 'react';
+import Card from '../Card';
+import styles from './styles.module.scss';
 import { _cs } from '@togglecorp/fujs';
-import Terminology from "../Terminology";
-import Loader from "../Loader";
+import Terminology from '../Terminology';
+import Loader from '../Loader';
 
-class QuestionCard extends React.Component {
-    constructor(props) {
-      super(props);
-        this.state = {
-            question: null,
-            term: null,
-            id: props.id,
-            parent_id: null,
-            options: [],
-            answer: null,
-            answerKey: null,
-            isLoading: true,
+function QuestionCard({ id, onSelectOption }) {
+    const [question, setQuestion] = useState(null);
+    const [term, setTerm] = useState(null);
+    const [parent_id, setParentId] = useState(null);
+    const [options, setOptions] = useState([]);
+    const [answer, setAnswer] = useState(null);
+    const [answerKey, setAnswerKey] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchState = async () => {
+            try {
+                const response = await fetch(`https://ifrc-sampling.azurewebsites.net/api/decision-tree/${id}/`);
+                const data = await response.json();
+                setQuestion(data.state.name);
+                setParentId(data.state.parent_state);
+                setOptions(data.options);
+                setTerm(data.state.term);
+            } catch (error) {
+                window.alert('An error occurred while fetching data.');
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
         };
-        this.fetchState = this.fetchState.bind(this);
-        this.handleOptionClick = this.handleOptionClick.bind(this);
-    }
 
-    componentDidMount() {
-        this.fetchState();
-    }
+        fetchState();
+    }, []);
 
-    fetchState() {
-        fetch('https://ifrc-sampling.azurewebsites.net/api/decision-tree/'+this.state.id+'/')
-            .then(response => response.json())
-            .then(data => 
-                this.setState({ 
-                question: data.state.name,
-                parent_id: data.state.parent_state,
-                options: data.options,
-                term: data.state.term,
-                }))
-            .then(() => this.setState({isLoading: false}));
-    }
+    const handleOptionClick = (term, option) => {
+        setAnswer(option.child_state);
+        setAnswerKey(option.child_state + option.name);
 
-    // pass child_state to parent (i.e., DecisionTree) to render next question card
-    handleOptionClick(term, option) {
-        this.setState({
+        onSelectOption({
             answer: option.child_state,
-            answerKey: option.child_state + option.name,},
-            () => this.props.onSelectOption(
-                {answer: this.state.answer,
-                id: this.state.id,
-                isHouseholds: option.name === "Households" ? true : false,
-                isSubgroup: term === "sub-population groups" && option.name === "Yes" ? true : false,}
-            )
-        );
-    }
+            id: id,
+            isHouseholds: option.name === 'Households' ? true : false,
+            isSubgroup: term === 'sub-population groups' && option.name === 'Yes' ? true : false,
+        });
+    };
 
-    render() {
-        const answer_key = this.state.answerKey;
-        const question = this.state.question;
-        const options = this.state.options;
-        const term = this.state.term;
-        return (
-            <>
-                {/* <img src={arrow} className={styles.arrow}/> */}
-                {this.state.isLoading ? <Loader/> :
-                    <Card>
-                        <h2> <Terminology term={term} text={question}/> </h2>
-                        <div className={styles.answers}>
-                            {options.map((option) => (
-                                <button 
-                                    key={option.id}
-                                    className={_cs(
-                                        styles.optionBtn,
-                                        option.child_state + option.name === answer_key
-                                        && styles.isActive,
-                                    )}
-                                    onClick={() => this.handleOptionClick(term, option)}>
-                                    {option.name}
-                                </button>
-                            ))}
-                        </div>
-                    </Card>
-                }
-            </>
-        );
-    }
+    return (
+        <>
+            {isLoading ? (
+                <Loader />
+            ) : 
+            (
+                <Card>
+                    <h2>
+                        <Terminology term={term} text={question} />
+                    </h2>
+                    <div className={styles.answers}>
+                        {options.map((option) => (
+                            <button
+                                key={option.id}
+                                className={_cs(styles.optionBtn, option.child_state + option.name === answerKey && styles.isActive)}
+                                onClick={() => handleOptionClick(term, option)}
+                            >
+                                {option.name}
+                            </button>
+                        ))}
+                    </div>
+                </Card>
+            )}
+        </>
+    );
 }
 
 export default QuestionCard;
