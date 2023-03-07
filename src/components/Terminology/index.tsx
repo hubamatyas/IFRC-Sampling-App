0d573ae87ded8ae373102472c89746ad83714897
Matyas Huba <hubamatyas@gmail.com>
2023-03-07 15:54:04 +0000

    Revert "off-canvas modified"
    This reverts commit f23fc7f3e1ee86275919bfd90a381a7a9b6c1a62.

--- src/components/Terminology/index.tsx
+++ src/components/Terminology/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { definitions } from "../Definitions";
-import OffCanvas from "../Off-canvas";
 
 interface Props {
   term: string | null;
@@ -22,14 +21,13 @@ const Terminology: React.FC<Props> = ({ term, text }: Props) => {
   return (
     <>
       {definition ? (
-           
-     <>
+        // add Offcanvas/Tooltip component when ready
+        <>
           {parts.length > 1 ? (
             <span>
               {parts.map((part) =>
                 part.toLowerCase() === term?.toLowerCase() ? (
-                  // <u key={part}>{part}</u>
-                  <OffCanvas terminology={part}/> 
+                  <u key={part}>{part}</u>
                 ) : (
                   part
                 )
