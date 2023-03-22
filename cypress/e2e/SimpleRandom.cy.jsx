08e686d3865b2d028ada8bc4a0fcbb4c6337601c
NickHe <zcabebx@ucl.ac.uk>
2023-03-22 16:37:20 +0000

    e2e test navbar

--- cypress/e2e/SimpleRandom.cy.jsx
+++ cypress/e2e/SimpleRandom.cy.jsx
@@ -20,7 +20,20 @@ describe('Test the simple random sampling calculator', () => {
 
   it('should show sample sizes for 3 subgroups', () => {
     cy.get("[data-cy='option-btn']").contains("Yes").click()
-    // customed command "inputSubgroupData" is defined in cypress/support/commands.js
+    cy.inputSubgroupData([10,20,30])
+    cy.get("[data-cy='submitgroups-btn']").click()
+
+    cy.get("[id='margin']").type("5")
+    cy.get("[id='response']").type("5")
+    cy.get("[id='confidence']").select("90")
+    cy.get("[id='confidence']").should("have.value", "90")
+    cy.get("[data-cy='submitCalculator-btn']").click()
+
+    cy.checkResult([11,20,29])
+  })
+
+  it('should show sample sizes for 3 subgroups', () => {
+    cy.get("[data-cy='option-btn']").contains("Yes").click()
     cy.inputSubgroupData([10,20,30])
     cy.get("[data-cy='submitgroups-btn']").click()
 
