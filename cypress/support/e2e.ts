64a96f6a0f0cef00e4d9d4c0472ff56100d9afe6
NickHe <zcabebx@ucl.ac.uk>
2023-03-23 03:41:56 +0000

    api testings

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-real-events/support'
+import 'cypress-plugin-api'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
